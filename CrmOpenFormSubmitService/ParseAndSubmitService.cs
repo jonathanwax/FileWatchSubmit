33920167c7c9b8ca748eba4d2a072ada96e8bd6c
Jonathan Wax <jwax@prodware.fr>
2016-12-25 16:07:59 +0200

    added win service

---
+++ CrmOpenFormSubmitService/ParseAndSubmitService.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmOpenFormSubmitService
+{
+    public partial class ParseAndSubmitService : ServiceBase
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public ParseAndSubmitService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            logger.Info("OnStart {args}", args);
+
+            //logger.Trace("Sample trace message");
+            //logger.Debug("Sample debug message");
+            //logger.Info("Sample informational message");
+            //logger.Warn("Sample warning message");
+            //logger.Error("Sample error message");
+            //logger.Fatal("Sample fatal error message");
+
+        }
+
+        protected override void OnStop()
+        {
+            logger.Info("OnStop");
+        }
+    }
+}
---
+++ CrmOpenFormSubmitService/install.cmd
@@ -0,0 +1,4 @@
+pause "CMD must be run as Administrator. Press Enter to continue..."
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil.exe" CrmOpenFormSubmitService.exe
+pause "Done"
+
